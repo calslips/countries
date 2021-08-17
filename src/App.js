1768986634fce33dff92307d416a80cbd497b0b5
Caleb Phillips <csphillips5@gmail.com>
2021-08-17 11:26:33 -0400

    implement useEffect to initialize state with  country data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then((response) => setCountries(response.data));
+  }, []);
+  console.log(countries);
+
+  const handleSearch = (event) => {
+    let query = event.target.value;
+    console.log(query);
+    setSearch(query);
+  }
+
   return (
     <div>
+      find countries <input value={search} onChange={handleSearch} />
     </div>
   );
 }
