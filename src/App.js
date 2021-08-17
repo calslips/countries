9ddddafd7efa713a5fa200b050c52926c7a12405
Caleb Phillips <csphillips5@gmail.com>
2021-08-17 12:01:57 -0400

    display search matches to the browser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const Matches = ({ results }) => {
+  return results.map((result) => <Match key={result.name} result={result} /> );
+};
+
+const Match = ({ result }) => (
+  <p>
+    {result.name}
+  </p>
+);
+
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then((response) => setCountries(response.data));
   }, []);
-  console.log(countries);
 
   const handleSearch = (event) => {
     let query = event.target.value;
     console.log(query);
     setSearch(query);
+    let result = countries.filter((country) =>
+      country.name.toUpperCase().includes(query.toUpperCase())
+    );
+    setResults(result);
   }
+
+  // const displayResult = () => {
+
+  // }
 
   return (
     <div>
       find countries <input value={search} onChange={handleSearch} />
+      <Matches results={results} />
     </div>
   );
 }
